10f3eee38ebad6d872e45b0e54d01685b85272ae
Dean <deancs1997@gmail.com>
2020-11-04 16:28:27 -0500

    client

---
+++ src/client/RssClient.java
@@ -0,0 +1,9 @@
+package client;
+
+public class RssClient {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("I'm from Client");
+    }
+}
