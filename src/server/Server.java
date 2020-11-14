8085c3c8b318b10837d13d9a44cac98eeb5d1a92
Dean <deancs1997@gmail.com>
2020-11-14 18:05:54 -0500

    final logger object

--- src/server/Server.java
+++ src/server/Server.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public class Server extends ServerModel implements ServerInterface {
-    private Logger logger;
+    private final Logger logger;
     List<ClientModel> clients;
     Communication communication;
 
