c53a3c5db2b3aa8f379617314f1a5bafe01e1d2d
Dean <deancs1997@gmail.com>
2020-11-14 17:42:42 -0500

    logging our usage

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,36 +1,42 @@
 package server;
 
 import communication.Communication;
+import logger.Logger;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
 public class Server extends ServerModel implements ServerInterface {
+    private Logger logger;
     List<ClientModel> clients;
     Communication communication;
 
     public Server(String connectionName) {
         this.clients = new LinkedList<ClientModel>();
+        this.logger = new Logger();
         this.communication = new Communication(connectionName);
     }
 
     @Override
     public void startServing() {
-
+        logger.log(super.getName() + "started serving");
     }
 
     @Override
     public void stopServing() {
-
+        logger.log(super.getName() + "stopped serving");
     }
 
     @Override
     public void registerUser(String requestNumber, String name, String ipAddress, int socketNumber) {
         if (!clientNameExist(name)) {
             this.clients.add(new ClientModel(name, ipAddress, socketNumber));
+            logger.log("ADDING USER", "User successfully added!");
+
             //TODO don't forget to send message
         }
+        logger.log("ADDING USER", "Chosen name already exists!");
         //TODO don't forget to send message
     }
 
@@ -38,8 +44,10 @@ public class Server extends ServerModel implements ServerInterface {
     public void deregisterUser(String requestNumber, String name) {
         if (clientNameExist(name)) {
             this.removeClientWithName(name);
+            logger.log("REMOVING USER", "User successfully deleted!");
             //TODO don't forget to send message
         }
+        logger.log("REMOVING USER", "User does not exist!");
         //TODO don't forget to send message
     }
 
@@ -53,9 +61,11 @@ public class Server extends ServerModel implements ServerInterface {
                 existingClient.setName(name);
                 existingClient.setIpAddress(ipAddress);
                 existingClient.setSocketNumber(socketNumber);
+                logger.log("UPDATING USER", "User successfully updated!");
             }
             //TODO don't forget to send message
         } else {
+            logger.log("UPDATING USER", "User does not exist!");
             //TODO don't forget to send message
         }
     }
@@ -67,18 +77,23 @@ public class Server extends ServerModel implements ServerInterface {
             Optional<ClientModel> ghostClient = this.clients.stream().filter(c -> c.getName().equals(name)).findFirst();
             if (ghostClient.isPresent()) {
                 existingClient = ghostClient.get();
-               //TODO: format list of subjects.
+                //TODO: format list of subjects.
+                logger.log("UPDATING SUBJECTS OF INTEREST", "Subjects of interest successfully updated!");
             }
             //TODO don't forget to send message
         } else {
             //TODO don't forget to send message
+            logger.log("UPDATING SUBJECTS OF INTEREST", "User does not exist!");
         }
     }
 
     @Override
     public void userPublishesMessage(String requestNumber, String name, String subject, String text) {
-        if (clientNameExist(name)){
+        if (clientNameExist(name)) {
             //TODO: send message to clients who subscriped to subject
+            logger.log("PUBLISHING MESSAGE", "Message published successfully!");
+        } else {
+            logger.log("PUBLISHING MESSAGE", "User does not exist!");
         }
     }
 
