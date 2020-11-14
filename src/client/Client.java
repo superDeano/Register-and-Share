17914d0ec1059c37c3320aac71b94a99fd468c80
Dean <deancs1997@gmail.com>
2020-11-14 18:05:45 -0500

    implemented way to make the client listen to message

--- src/client/Client.java
+++ src/client/Client.java
@@ -2,18 +2,27 @@ package client;
 
 import communication.Communication;
 import server.ClientModel;
+import server.ServerModel;
 
 import java.util.Vector;
 
 public class Client extends ClientModel implements ClientInterface{
     private Communication communication;
+    private final ServerModel[] servers = new ServerModel[2];
+    private int servingServer = -1;
 
     public Client(){
         super();
+        this.servers[0] = new ServerModel("server one");
+        this.servers[1] = new ServerModel("server two");
+        this.communication = new Communication("client");
     }
 
     public Client(String name, String ipAddress, int socketNumber) {
         super(name, ipAddress, socketNumber);
+        this.servers[0] = new ServerModel("server one");
+        this.servers[1] = new ServerModel("server two");
+        this.communication = new Communication("client");
     }
 
     @Override
@@ -38,12 +47,21 @@ public class Client extends ClientModel implements ClientInterface{
 
     @Override
     public void updateInformationToServer() {
-
+//        communication.sendMessage();
     }
 
     @Override
     public void publishMessage(String topic, String message) {
 
+    }
+
+    public ServerModel[] getServers(){
+        return this.servers;
+    }
+
+    public String listen(){
+        //TODO check what the message is and take appropriate action
+        return communication.waitForMessage();
     }
 
 }
