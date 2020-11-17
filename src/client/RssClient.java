2b66e8d656c58bc4afda75f647bfff9825831855
Dean <deancs1997@gmail.com>
2020-11-16 22:04:40 -0500

    using bounds to fix the ui placements

--- src/client/RssClient.java
+++ src/client/RssClient.java
@@ -2,9 +2,11 @@ package client;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 //import java.awt.*;
 
-public class RssClient {
+public class RssClient implements ActionListener {
     private static JFrame frame;
     private static JPanel clientPanel, serverPanel, topicPanel, messagePannel, logsPanel;
     private static JButton registerButton, deregisterButton, publishButton, updateClientPortNumberButton;
@@ -24,7 +26,7 @@ public class RssClient {
         frame = new JFrame("Register And Share");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 400);
-
+//        frame.setLayout(null);
         //Buttons
         registerButton = new JButton("Register");
         deregisterButton = new JButton("Deregister");
@@ -40,11 +42,11 @@ public class RssClient {
         server2PortNumber = new JTextField(1);
         GridBagConstraints c = new GridBagConstraints();
 
-        setClientPanel(c);
-        setServerPanel(c);
-        setTopicPanel(c);
-        setMessagePanel(c);
-        setLogsPanel(c);
+        setClientPanel();
+        setServerPanel();
+        setTopicPanel();
+        setMessagePanel();
+        setLogsPanel();
 
         setTabbedPane();
 
@@ -52,79 +54,88 @@ public class RssClient {
         frame.setVisible(true);
     }
 
-    private static void setClientPanel(GridBagConstraints c) {
+    private static void setClientPanel() {
         //Client Panel
-        clientPanel = new JPanel(new BorderLayout());
-        clientPanel.setLayout(new GridBagLayout());
+        clientPanel = new JPanel();
+        clientPanel.setLayout(null);
+
+
+        JLabel clientIp = new JLabel("IP Address");
+        clientIp.setBounds(10, 10, 100, 20);
+        clientPanel.add(clientIp);
+
+        // Label that shows the client IP Address
+        clientIpAddress.setBounds(160, 10, 100, 20);
+        clientPanel.add(clientIpAddress);
+
+        JLabel clientPn = new JLabel("Client Port Number");
+        clientPn.setBounds(10, 40, 150, 20);
+        clientPanel.add(clientPn);
+
+        // Text Field for client Port Number
+        clientPortNumber.setBounds(160, 40, 50, 20);
+        clientPanel.add(clientPortNumber);
+
         updateClientPortNumberButton = new JButton("Update Port Number");
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        c.gridy = 0;
-        clientPanel.add(new JLabel("IP Address"), c);
-        c.gridy = 1;
-        clientPanel.add(clientIpAddress, c);
-        c.gridy = 2;
-        clientPanel.add(new JLabel("Client Port Number"), c);
-        c.gridy = 4;
-        clientPanel.add(clientPortNumber, c);
-        c.gridy = 5;
-        clientPanel.add(updateClientPortNumberButton, c);
+        updateClientPortNumberButton.setBounds(10, 75, 150, 20);
+        clientPanel.add(updateClientPortNumberButton);
     }
 
-    private static void setServerPanel(GridBagConstraints c) {
+    private static void setServerPanel() {
         //Server Panel
+        int textLabelWidth = 150;
         serverPanel = new JPanel(new BorderLayout());
-        serverPanel.setLayout(new GridBagLayout());
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 1;
-        c.gridy = 0;
-        c.weightx = 0.5;
-        serverPanel.add(new JLabel("Server 1 IP Address"), c);
-        c.gridy = 1;
-        c.gridwidth = 2;
-        serverPanel.add(server1IpAddress, c);
-        c.gridy = 2;
-        c.gridwidth = 1;
-        serverPanel.add(new JLabel("Server 1 Port Number"), c);
-        c.gridy = 3;
-        c.gridwidth = 2;
-        serverPanel.add(server1PortNumber, c);
-        c.gridy = 4;
-        c.gridwidth = 1;
-        serverPanel.add(new JLabel("Server 2 IP Address"), c);
-        c.gridy = 5;
-        c.gridwidth = 2;
-        serverPanel.add(server2IpAddress, c);
-        c.gridy = 6;
-        c.gridwidth = 1;
-        serverPanel.add(new JLabel("Server 2 Port Number"), c);
-        c.gridy = 7;
-        c.gridwidth = 2;
-        serverPanel.add(server2PortNumber, c);
-        c.gridy = 9;
-        c.gridwidth = 1;
-        c.gridx = 0;
-        serverPanel.add(registerButton, c);
-        c.gridy = 9;
-        c.gridx= 2;
-        serverPanel.add(deregisterButton, c);
+        serverPanel.setLayout(null);
+        JLabel server1IpAddressLabel = new JLabel("Server 1 IP Address");
+        server1IpAddressLabel.setBounds(10, 10, textLabelWidth, 20);
+        serverPanel.add(server1IpAddressLabel);
+
+        server1IpAddress.setBounds(textLabelWidth + 20, 10, textLabelWidth + 50, 20);
+        serverPanel.add(server1IpAddress);
+
+        JLabel server1PnLabel = new JLabel("Server 1 Port Number");
+        server1PnLabel.setBounds(10, 40, textLabelWidth, 20);
+        serverPanel.add(server1PnLabel);
+
+        server1PortNumber.setBounds(textLabelWidth + 20, 40, textLabelWidth + 50, 20);
+        serverPanel.add(server1PortNumber);
+
+        JLabel server2IpAddressLabel = new JLabel("Server 2 IP Address");
+        server2IpAddressLabel.setBounds(10, 70, textLabelWidth, 20);
+        serverPanel.add(server2IpAddressLabel);
+
+        server2IpAddress.setBounds(textLabelWidth + 20, 70, textLabelWidth + 50, 20);
+        serverPanel.add(server2IpAddress);
+
+        JLabel server2PnLabel = new JLabel("Server 2 Port Number");
+        server2PnLabel.setBounds(10, 100, textLabelWidth, 20);
+        serverPanel.add(server2PnLabel);
+
+        server2PortNumber.setBounds(20 + textLabelWidth, 100, textLabelWidth + 50, 20);
+        serverPanel.add(server2PortNumber);
+
+        registerButton.setBounds(10, 160, 100, 20);
+        serverPanel.add(registerButton);
+
+        deregisterButton.setBounds(120, 160, 100, 20);
+        serverPanel.add(deregisterButton);
     }
 
 
-    private static void setTopicPanel(GridBagConstraints c) {
+    private static void setTopicPanel() {
         //Topic Panel
         topicPanel = new JPanel();
         topicPanel.add(new JLabel("Topics you're subscribed"));
     }
 
 
-    private static void setMessagePanel(GridBagConstraints c) {
+    private static void setMessagePanel() {
         //Messages Panel
         messagePannel = new JPanel();
         messagePannel.add(publishButton);
     }
 
-    private static void setLogsPanel(GridBagConstraints c) {
+    private static void setLogsPanel() {
         //Logs Panel
         logsPanel = new JPanel();
     }
@@ -137,5 +148,10 @@ public class RssClient {
         tabbedPane.add("Topics", topicPanel);
         tabbedPane.add("Message", messagePannel);
         tabbedPane.add("Event Logs", logsPanel);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Something happened");
     }
 }
