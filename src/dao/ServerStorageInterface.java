4fd80b714251051876b7d976d5df9b06f8759854
Dean <deancs1997@gmail.com>
2020-11-21 14:31:01 -0500

    added methods for the server storage service

--- src/dao/ServerStorageInterface.java
+++ src/dao/ServerStorageInterface.java
@@ -1,2 +1,17 @@
-package dao;public interface ServerStorageInterface {
+package dao;
+
+import server.ClientModel;
+import server.ServerModel;
+
+import java.util.List;
+
+public interface ServerStorageInterface {
+    public void addClient(ClientModel client);
+    public void updateClientInfo(ClientModel client);
+    public void deleteClient(String name);
+    public List<ClientModel> getAllClients();
+    public void clientSubscribeToSubjects(String clientName, List<String> subjects);
+    public void clientUnsubscribeToSubjects(String clientName, List<String> subjects);
+    public void updateOtherServerIpAddressAndPortNumber(String ipAddress, int portNumber);
+    public ServerModel getOtherServerInfo();
 }
